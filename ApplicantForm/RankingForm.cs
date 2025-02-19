86feacf609bee61c97222fae1ed93459874d667d
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-02-19 20:35:52 +0300

    WIP (RankingForm)
    Начинаю разбираться с dataGridView для того чтобы красиво выводить таблицу в форме там и будут у меня отсортированные студенты

---
+++ ApplicantForm/RankingForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AdmissionCommitteeBackend;
+
+namespace ApplicantForm
+{
+    public partial class RankingForm : Form
+    {
+        private AdmissionCommitteeService _service;
+
+        public RankingForm(AdmissionCommitteeService service)
+        {
+            InitializeComponent();
+            _service = service;
+            LoadRanking();
+        }
+
+        private void LoadRanking()
+        {
+            var ranking = _service.GetApplicantsRanking();
+            dataGridView.DataSource = ranking;
+        }
+    }
+}
