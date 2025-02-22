e45b4e6874aad0f1e249a8a2a6f39b6c6b667948
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-02-22 12:18:57 +0300

    WIP

--- ApplicantLibrary/DataBaseHelper.cs
+++ ApplicantLibrary/DataBaseHelper.cs
@@ -4,7 +4,7 @@ namespace AdmissionCommitteeBackend
 {
     public class DatabaseHelper
     {
-        private const string ConnectionString = "C:\\Users\\Дмитрий\\Documents\\GitHub\\ApplicantProject\\ApplicantLibrary\\Базы данных\\ApplicantDB.sqbpro"; //Тут должна быть ссылка на базу данных
+        private const string ConnectionString = "C:\\Users\\Дмитрий\\Documents\\GitHub\\ApplicantProject\\ApplicantLibrary\\Базы данных\\ApplicantDB.db"; //Тут должна быть ссылка на базу данных
 
         public static void InitializeDatabase()
         {
